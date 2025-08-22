99b33b385e9146857a3cb9efe8536d19d91ed79b
Subhrodeep Acharya <subhrodeepacharya14@gmail.com>
2025-08-22 11:32:51 +0530

    db connect

--- app/api/clerk/webhook/route.ts
+++ app/api/clerk/webhook/route.ts
@@ -1,8 +1,22 @@
+import { db } from "@/server/db";
 
 
 export const POST =async(req:Request)=>{
     const {data}=await req.json();
     console.log("clerk webhook",data);
+    const email =data.email_Addresses[0].email_Address;
+    const name =data.first_Name;
+    const image =data.image_url;
+    const id = data.id;
+
+    await db.user.create({
+        data:{
+            email,
+            name,
+            image,
+            id
+        }
+    });
     return new Response("webhook received",{status:200});
 
 }
